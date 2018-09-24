8c246622cc672842d37ed846e1490e60d190a14c
Jared Cuffe <jcuffe@gmail.com>
2018-09-24 11:19:36 -0700

    Redux action flow to verify and store token

--- src/components/auth/redirectHandler.js
+++ src/components/auth/redirectHandler.js
@@ -1,8 +1,14 @@
-import React from 'react';
+// import React from 'react';
 import qs from 'qs';
+import { connect } from 'react-redux';
+import { verifyToken } from '../../actions';
 
-export default ({ location }) => {
-  console.log(location);
-  console.log(qs.parse(location.hash.slice(1)));
-  return <div>{location.hash}</div>;
+const redirectHandler = ({ location, history, verifyToken }) => {
+  const { access_token: accessToken } = qs.parse(location.hash.slice(1))
+  verifyToken(accessToken);
+  history.push('');
+  return null;
 };
+
+const dispatchToProps = { verifyToken };
+export default connect(null, dispatchToProps)(redirectHandler);
