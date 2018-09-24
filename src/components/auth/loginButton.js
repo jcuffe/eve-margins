3c4e20ff7879079e33877426c3ef1e80f7e75c07
Jared Cuffe <jcuffe@gmail.com>
2018-09-24 11:21:50 -0700

    Login button URLs refactored and has image

--- src/components/auth/loginButton.js
+++ src/components/auth/loginButton.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import qs from 'qs';
+import { images, urls } from '../../urls';
 
 export default (props) => {
-  const baseUrl = process.env.REACT_APP_AUTHORIZE_URL;
   const params = qs.stringify({
     response_type: "token",
-    redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+    redirect_uri: urls.oauthRedirect,
     client_id: process.env.REACT_APP_CLIENT_ID,
     scope: process.env.REACT_APP_OAUTH_SCOPES,
     state: ""
   });
-  console.log(params);
-  const compositeUrl = [baseUrl, params].join("?");
+  const href = [urls.authorization, params].join("?");
   return (
-    <a href={compositeUrl}>
-      Click
+    <a href={href}>
+      <img src={images.loginButton} alt="Login" />
     </a>
   );
 };
