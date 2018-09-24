b8662b74ac09ff108658248b1172944dd1c8bb48
Jared Cuffe <jcuffe@gmail.com>
2018-09-24 11:18:53 -0700

    Moved URLs from secret location to normal file

---
+++ src/urls.js
@@ -0,0 +1,14 @@
+const images = {
+  loginButton: "https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-small.png"
+};
+
+const urls = {
+  tokenVerification: "https://esi.tech.ccp.is/verify/",
+  oauthRedirect: "http://localhost:3000/authorization-redirect/",
+  authorization: "https://login.eveonline.com/oauth/authorize/",
+};
+
+export {
+  urls,
+  images
+};
