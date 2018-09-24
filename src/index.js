f66802a07c13032f86ec932b4cbe3df02202c510
Jared Cuffe <jcuffe@gmail.com>
2018-09-24 08:27:07 -0700

    Redux initialization; Subscription on store to save token locally

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
 import App from './components/app';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, undefined, applyMiddleware(thunkMiddleware));
+
+store.subscribe(() => localStorage.setItem('TOKEN', store.getState().authToken));
 
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
