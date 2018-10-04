39e5f2fb110332a820a99674c14973fe3504fe47
Jared Cuffe <jcuffe@gmail.com>
2018-10-03 18:36:58 -0700

    Import two missing functions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import { setToken, setCharacter } from './dispatch';
 import { urls } from '../urls';
 
 export const verifyToken = (token) => async (dispatch) => {
   try {
     console.log('preflight', token);
-    const { data: { CharacterID: id } } = await axios.get(urls.tokenVerification, {
-      headers: { Authorization: `Bearer ${token}` }
-    });
+      const { data: { CharacterID: id } } = await axios.get(urls.tokenVerification, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
     dispatch(setToken(token));
     dispatch(setCharacter({ id }));
   } catch (error) {
