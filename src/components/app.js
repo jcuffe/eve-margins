08cf39059d691b8bdc954bc560d6e96048efaf10
Jared Cuffe <jcuffe@gmail.com>
2018-10-03 18:41:05 -0700

    Fetches searchable names and IDs of systems

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import LoginButton from './auth/loginButton';
+import Autocomplete from './autocomplete';
 import RedirectHandler from './auth/redirectHandler';
+import { fetchSystems } from '../actions/universe';
+import { handleInput } from '../actions/input';
 
-const app = (props) => (
-  <div>
-    <Route path="/" component={LoginButton} />
-    <Route path="/authorization-redirect" component={RedirectHandler} />
-  </div>
-)
+function app({ fetchSystems, systems, input, handleInput }) {
+  return (
+    <div>
+      <Route path="/" component={LoginButton} />
+      <button onClick={fetchSystems}>Fetch Systems</button>
+      <Autocomplete 
+        datasource={Object.values(systems)} 
+        sourceKey="name" />
+      
+      <Route path="/authorization-redirect" component={RedirectHandler} />
+    </div>
+  );
+}
 
-export default app;
+function stateToProps(state) {
+  return {
+    systems: state.systems,
+    input: state.input
+  }
+}
+
+const dispatchToProps = { fetchSystems, handleInput };
+
+export default connect(stateToProps, dispatchToProps)(app);
