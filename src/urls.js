4a7e2d335e4a7effbabc5aaaafb262a2910e1485
Jared Cuffe <jcuffe@gmail.com>
2018-10-03 18:36:06 -0700

    Added ESI endpoints

--- src/urls.js
+++ src/urls.js
@@ -6,9 +6,21 @@ const urls = {
   tokenVerification: "https://esi.tech.ccp.is/verify/",
   oauthRedirect: "http://localhost:3000/authorization-redirect/",
   authorization: "https://login.eveonline.com/oauth/authorize/",
+  api: "https://esi.evetech.net/latest/"
+};
+
+const endpoints = {
+  regions: urls.api + "universe/regions/",
+  constellations: urls.api + "universe/constellations/",
+  systems: urls.api + "universe/systems/",
+  names: urls.api + "universe/names/?datasource=tranquility",
+  region: (id) => urls.api + `universe/regions/${id}`,
+  constellation: (id) => urls.api + `universe/constellations/${id}`,
+  system: (id) => urls.api + `universe/systems/${id}`
 };
 
 export {
   urls,
-  images
+  images,
+  endpoints
 };
