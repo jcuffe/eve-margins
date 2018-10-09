095af343cf89a7b4e9679df721bc000e54255338
Jared Cuffe <jcuffe@gmail.com>
2018-10-09 10:36:07 -0700

    Hardcoding interesting IDs instead of fetching all entities
    To fetch all public structures and correlate  with systems requires  many requests. Instead we will identify the most important Systems/Stations/Structures, and allow the user to add their own if desired.

---
+++ src/seeds.js
@@ -0,0 +1,17 @@
+export const systemIds = [
+  30004759, // 1dq1-a
+  30000142, // Jita
+];
+
+export const structureIds = [
+  1022734985679, // 1dq Thetastar
+];
+
+export const stationIds = [
+  60003760, // Jita 4-4
+]
+
+export const itemIds = [
+  // Fuel Blocks
+  4246, // Hydrogen  
+]; 
