e92afb556671032a6e9a9cdbcc22ab242db12c29
Jared Cuffe <jcuffe@gmail.com>
2018-10-09 08:18:07 -0700

    Structure actions, utility function for wrapping headers

--- src/actions/dispatch.js
+++ src/actions/dispatch.js
@@ -1,10 +1,18 @@
 export const type = {
+  ADD_STRUCTURE: 'add structure',
   AUTHORIZE: 'authorize',
   LOGOUT: 'logout',
   SET_CHARACTER: 'set char',
   SET_SYSTEMS: 'set systems',
+  SET_STRUCTURES: 'set structures',
   SET_INPUT: 'set input',
 };
+
+export const authHeaders = (token) => ({
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
 
 export const setToken = (token) => ({
   type: type.AUTHORIZE,
@@ -19,6 +27,16 @@ export const setCharacter = (data) => ({
 export const setSystems = (systems) => ({
   type: type.SET_SYSTEMS,
   payload: systems
+});
+
+export const setStructures = (structures) => ({
+  type: type.SET_STRUCTURES,
+  payload: structures
+});
+
+export const addStructure = (structure) => ({
+  type: type.ADD_STRUCTURE,
+  payload: structure
 });
 
 export const setInput = (value) => ({
