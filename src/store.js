44b42c6f1a346df9faae5323e33884b8d07c896f
Jared Cuffe <jcuffe@gmail.com>
2018-10-09 10:33:45 -0700

    Streamlined state persistence. TODO: Load state

--- src/store.js
+++ src/store.js
@@ -1,20 +1,15 @@
-import rootReducer from './reducers';
+import rootReducer from './reducers/root';
 import thunk from 'redux-thunk';
 import { applyMiddleware, createStore } from 'redux';
 
-function persistState(store, keys) {
-  console.log(keys);
+function persistState(store) {
   let currentState;
-
-  function persist(key, val) {
-    localStorage.setItem(key, JSON.stringify(val));
-  }
 
   function handleChange() {
     const nextState = store.getState();
     if (nextState !== currentState) {
       currentState = nextState;
-      keys.forEach(key => persist(key, nextState[key]));
+      localStorage.setItem("state", JSON.stringify(currentState));
     }
   }
 
@@ -24,7 +19,6 @@ function persistState(store, keys) {
 }
 
 const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
-const persistedKeys = process.env.REACT_APP_PERSISTED_KEYS.split(" ");
-persistState(store, persistedKeys);
+persistState(store);
 
 export default store;
