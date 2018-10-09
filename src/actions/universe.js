08f216a1248d3dd31ba384ee2d5eb945fbbd9228
Jared Cuffe <jcuffe@gmail.com>
2018-10-09 08:18:21 -0700

    Fetch a small subset of structures

--- src/actions/universe.js
+++ src/actions/universe.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
-import { setSystems } from './dispatch';
+import { setSystems, authHeaders, setStructures } from './dispatch';
 import { endpoints } from '../urls';
 
 export const fetchSystems = () => async (dispatch) => {
+  console.log("Fetching systems");
   try {
     const systemIds = await getIds(endpoints.systems);
     const systems = await namesFromIds(systemIds);
     dispatch(setSystems(systems));
     console.log("systems", systems);
   } catch (error) {
+    console.log("Failed to fetch systems");
+  }
+};
+
+export const fetchStructures = (ids, token) => async (dispatch) => {
+  console.log(`Fetching ${ids.length} structures`);
+  try {
+    const options = authHeaders(token);
+    const requests = ids.map(id => axios.get(endpoints.structure(id), options));
+    const data = (await Promise.all(requests))
+      .map(response => response.data)
+    dispatch(setStructures(data));
+  } catch (error) {
+    console.log("Failed to fetch structure info.");
     console.log(error);
   }
 };
@@ -26,4 +41,4 @@ async function namesFromIds(ids) {
     data.forEach(sys => systems[sys.id] = { name: sys.name, id: sys.id });
   }
   return systems;
-} 
+}
