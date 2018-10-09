c6a9421341a5398a72cb6a64176bd3f05a7febd8
Jared Cuffe <jcuffe@gmail.com>
2018-10-09 10:37:38 -0700

    Moving logic to child components

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import LoginButton from './auth/loginButton';
-import Autocomplete from './autocomplete';
-import RedirectHandler from './auth/redirectHandler';
-import { fetchSystems } from '../actions/universe';
-import { handleInput } from '../actions/input';
 
-function app({ fetchSystems, systems, input, handleInput }) {
+import LoginButton from './auth/loginButton';
+import RedirectHandler from './auth/redirectHandler';
+import Structures from './structures';
+import Systems from './systems';
+
+function app({ character, token }) {
   return (
     <div>
       <Route path="/" component={LoginButton} />
-      <button onClick={fetchSystems}>Fetch Systems</button>
-      <Autocomplete 
-        datasource={Object.values(systems)} 
-        sourceKey="name" />
-      
+      <p>token: {token}</p>
+      <p>Character ID: {character.id}</p>
+      <Structures />
+      <Systems />
       <Route path="/authorization-redirect" component={RedirectHandler} />
     </div>
   );
@@ -23,11 +22,9 @@ function app({ fetchSystems, systems, input, handleInput }) {
 
 function stateToProps(state) {
   return {
-    systems: state.systems,
-    input: state.input
+    token: state.authToken,
+    character: state.character
   }
 }
 
-const dispatchToProps = { fetchSystems, handleInput };
-
-export default connect(stateToProps, dispatchToProps)(app);
+export default connect(stateToProps)(app);
