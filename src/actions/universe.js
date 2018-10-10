68a052610c227a7655f56bb3aa69fa0077902422
Jared Cuffe <jcuffe@gmail.com>
2018-10-10 14:03:38 -0700

    Refactor ESI data fetching

--- src/actions/universe.js
+++ src/actions/universe.js
@@ -1,32 +1,47 @@
 import axios from 'axios';
-import { setSystems, authHeaders, setStructures } from './dispatch';
+import { setSystems, authHeaders, setStructures, setConstellations, failed, fetching } from './dispatch';
 import { endpoints } from '../urls';
 
-export const fetchSystems = (ids) => async (dispatch) => {
-  console.log("Fetching systems");
-  try {
-    const requests = ids.map(id => axios.get(endpoints.system(id)));
+const fetchData = async (ids, endpoint, options = {}) => {
+    const requests = ids.map(id => axios.get(endpoint(id), options));
     const data = (await Promise.all(requests))
       .map(response => response.data)
-      .reduce((systems, current) => ({ [current.system_id]: current, ...systems }), {});
-    dispatch(setSystems(data));
-    console.log("systems", data);
-  } catch (error) {
-    console.log("Failed to fetch systems");
-  }
+      .reduce((dictionary, element, i) => ({ ...dictionary, [ids[i]]: element }), {});
+    return data;
 };
 
-export const fetchStructures = (ids, token) => async (dispatch) => {
-  console.log(`Fetching ${ids.length} structures`);
+export const fetchSystems = (ids) => async (dispatch) => {
+    dispatch(fetching("systems"));
+    try {
+      const systems = await fetchData(ids, endpoints.system);    
+      dispatch(setSystems(systems));
+  
+      // Identify constellations for fetched systems
+      const constellationIds = new Set();
+      Object.values(systems).forEach(system => constellationIds.add(system.constellation_id));
+      dispatch(fetchConstellations(Array.from(constellationIds)));
+    } catch (error) {
+      dispatch(failed("systems"));
+    }
+};
+
+const fetchConstellations = (ids) => async (dispatch) => {
+  dispatch(fetching("constellations"));
   try {
-    const options = authHeaders(token);
-    const requests = ids.map(id => axios.get(endpoints.structure(id), options));
-    const data = (await Promise.all(requests))
-      .map(response => response.data)
-      .reduce((structures, current) => ({ [current.solar_system_id]: current, ...structures }), {});
-    dispatch(setStructures(data));
+    const constellations = await fetchData(ids, endpoints.constellation);
+    dispatch(setConstellations(constellations));
   } catch (error) {
-    console.log("Failed to fetch structure info.");
-    console.log(error);
+    dispatch(failed("constellations"));
+  }
+}
+
+export const fetchStructures = (ids, token) => async (dispatch) => {
+  dispatch(fetching("structures"));
+  try {
+    const options = authHeaders(token); // Authorization required
+    const structures = await fetchData(ids, endpoints.structure, options);
+    dispatch(setStructures(structures));
+  } catch (error) {
+    dispatch(failed("structures"));
   }
 };
