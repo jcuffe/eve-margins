0930efcc30180b5eff1b4988adf05d4ebcc64e13
Jared Cuffe <jcuffe@gmail.com>
2018-10-24 21:47:45 -0700

    Adds button to test market data fetching

--- src/components/market/results.js
+++ src/components/market/results.js
@@ -1,3 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { getActiveTypes } from '../../actions/dispatch';
 
-export default () => (<div></div>);
+const Results = ({ getActiveTypes }) => (
+  <div>
+    <button onClick={getActiveTypes}>Get Active Types in The Forge</button>
+  </div>
+);
+
+const dispatchToProps = { getActiveTypes };
+
+export default connect(null, dispatchToProps)(Results);
