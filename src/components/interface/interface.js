254f51c1b4a5f3ab48b0ab916b406f4ba0fc27ac
Jared Cuffe <jcuffe@gmail.com>
2018-10-27 18:37:27 -0700

    Refactors all logic out of the interface component

--- src/components/interface/interface.js
+++ src/components/interface/interface.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import LoginButton from '../auth/loginButton';
 import MarketLocations from '../market/locations';
 import MarketFilters from '../market/filters';
 import MarketTree from '../market/tree';
 import MarketResults from '../market/results';
 import Characters from './characters';
 
-import { selectCharacter } from '../../actions/dispatch';
-
-const Interface = ({ characters, selectCharacter }) => (
-  <div>
-    <LoginButton />
+export default () => (
+  <div className="interface">
     <Characters />
+    <MarketTree />
     <MarketLocations />
     <MarketFilters />
-    <MarketTree />
     <MarketResults />
   </div>
 );
-
-const stateToProps = (state) => ({
-  characters: state.characters
-});
-
-const dispatchToProps = { selectCharacter };
-
-export default connect(stateToProps, dispatchToProps)(Interface);
