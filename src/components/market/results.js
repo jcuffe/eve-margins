da70aadec1c0c6da523590f2b7887a7dbef105d1
Jared Cuffe <jcuffe@gmail.com>
2018-10-27 18:39:22 -0700

    Improves performance of types and orders lists using virtualization
    I love it!

--- src/components/market/results.js
+++ src/components/market/results.js
@@ -1,37 +1,55 @@
 import React from 'react';
+import { Table, Column, AutoSizer } from 'react-virtualized';
 import { connect } from 'react-redux';
 import { getActiveTypes } from '../../actions/dispatch';
 
-const Results = ({ getActiveTypes, orders }) => (
-  <div>
-    <button onClick={getActiveTypes}>Get Active Types in The Forge</button>
-    <table>
-      <thead>
-        <tr>
-          <th>Type ID</th>
-          <th>Price</th>
-          <th>Buy Order?</th>
-          <th>Location ID</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.values(orders).map(typeOrders => (
-          typeOrders.map(order => (
-            <tr>
-              <td>{order.type_id}</td>
-              <td>{order.price}</td>
-              <td>{order.is_buy_order ? "Yes" : "No"}</td>
-              <td>{order.location_id}</td>
-            </tr>
-          )) 
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+const Results = ({ getActiveTypes, orders, types }) => {
+  const rowGetter = ({ index }) => orders[index];
+  const orderTypeRenderer = ({ cellData }) => cellData ? "Buy" : "Sell";
+  const typeNameRenderer = ({ cellData }) => types[cellData] ? types[cellData].typeName : "???";
+  return (
+    <div className="market-results">
+      <button onClick={getActiveTypes}>Get Active Types in The Forge</button>
+      <AutoSizer disableHeight>
+        {({width}) => (
+          <Table
+            ref="Table"
+            headerHeight={50}
+            height={800}
+            noRowsRenderer={() => <div></div>}
+            overscanRowCount={10}
+            rowHeight={40}
+            rowGetter={rowGetter}
+            rowCount={orders.length}
+            width={width}>
+            <Column
+              label="Type Name"
+              dataKey="type_id"
+              cellRenderer={typeNameRenderer}
+              width={300}
+              flexGrow={1}
+            />
+            <Column
+              label="Order Type"
+              dataKey="is_buy_order"
+              cellRenderer={orderTypeRenderer}
+              width={100}
+            />
+            <Column
+              label="Price"
+              dataKey="price"
+              width={150}
+            />
+          </Table>
+        )}
+      </AutoSizer>
+    </div>
+  );
+};
 
 const stateToProps = (state) => ({
-  orders: state.market.orders
+  orders: [].concat(...Object.values(state.market.orders)),
+  types: state.types
 });
 
 const dispatchToProps = { getActiveTypes };
--- src/components/market/tree.js
+++ src/components/market/tree.js
@@ -1,3 +1,50 @@
 import React from 'react';
+import { List, AutoSizer } from 'react-virtualized';
+import { connect } from 'react-redux';
+import { getTypes, selectType } from '../../actions/dispatch';
+import 'react-virtualized/styles.css'
 
-export default () => (<div></div>);
+const Type = ({ type, selectType, key, style }) => {
+  const onClick = () => selectType(type.typeID);
+  return (
+    <div {...{ onClick, key, style }} >
+      {type.typeName}
+    </div>
+  );
+}; 
+
+
+const Tree = ({ types, getTypes, selectType }) => {
+  const rowRenderer = ({ key, index, style }) => (
+    <Type {...{ type: types[index], key, style, selectType }} />
+  );
+  if (types.length === 0) {
+    getTypes();
+  }
+  return (
+    <div className="type-tree">
+      <AutoSizer disableHeight>
+        {({width}) => (
+          <List
+            ref="List"
+            height={800}
+            noRowsRenderer={() => <div></div>}
+            overscanRowCount={10}
+            rowCount={types.length}
+            rowHeight={50}
+            rowRenderer={rowRenderer}
+            width={width}
+          />
+        )}
+      </AutoSizer>
+    </div>
+  );
+};
+
+const stateToProps = (state) => ({
+  types: Object.values(state.types) // Not ideal
+});
+
+const dispatchToProps = { getTypes, selectType };
+
+export default connect(stateToProps, dispatchToProps)(Tree);
