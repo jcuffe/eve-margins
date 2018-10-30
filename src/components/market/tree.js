95f30db41a376ee54f0d14693a91776172f24d45
Jared Cuffe <jcuffe@gmail.com>
2018-10-30 13:30:26 -0700

    Adds type selection to the types tree

--- src/components/market/tree.js
+++ src/components/market/tree.js
@@ -1,46 +1,51 @@
 import React from 'react';
+import classNames from 'classnames';
 import { useState } from 'react';
 import { List, AutoSizer } from 'react-virtualized';
 import { connect } from 'react-redux';
 import { getTypes, selectType } from '../../actions/dispatch';
 import 'react-virtualized/styles.css'
 
-const Type = ({ type, selectType, key, style }) => {
+const Type = ({ type, selected, selectType, key, style }) => {
   const onClick = () => selectType(type.typeID);
+  const className = classNames({
+    "selected": selected
+  })
   return (
-    <div {...{ onClick, key, style }} >
+    <div {...{ className, onClick, key, style }} >
       {type.typeName}
     </div>
   );
 }; 
 
 
-const Tree = ({ types, getTypes, selectType }) => {
+const Tree = ({ types, selectedType, getTypes, selectType }) => {
   const [filter, setFilter] = useState("");
   const filteredTypes = types.filter(type => type.typeName.toLowerCase().includes(filter));
   const handleChange = (e) => setFilter(e.target.value.toLowerCase());
   const rowRenderer = ({ key, index, style }) => {
     const type = filteredTypes[index];
     if (!type) return null;
-    return <Type {...{ type, key, style, selectType }} />;
+    const selected = type.typeID === selectedType;
+    return <Type {...{ type, key, style, selected, selectType }} />;
   };
   if (types.length === 0) {
     getTypes();
   }
   return (
-    <div className="type-tree">
+    <div className="types-tree">
       <input value={filter} onChange={handleChange} />
-      <AutoSizer disableHeight>
-        {({width}) => (
+      <AutoSizer>
+        {({width, height}) => (
           <List
             ref="List"
-            height={800}
+            width={width}
+            height={height}
             noRowsRenderer={() => <div></div>}
-            overscanRowCount={10}
+            overscanRowCount={0}
             rowCount={types.length}
             rowHeight={50}
             rowRenderer={rowRenderer}
-            width={width}
           />
         )}
       </AutoSizer>
@@ -49,7 +54,8 @@ const Tree = ({ types, getTypes, selectType }) => {
 };
 
 const stateToProps = (state) => ({
-  types: Object.values(state.types) // Not ideal
+  types: Object.values(state.types.all),
+  selectedType: state.types.selected // Not ideal
 });
 
 const dispatchToProps = { getTypes, selectType };
--- src/modules/types.js
+++ src/modules/types.js
@@ -1,11 +1,12 @@
 import { switchMap, map, tap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
+import { combineReducers } from 'redux';
 import { ofType } from 'redux-observable';
 import { type } from '../actions/dispatch';
 import { setTypes } from '../actions/dispatch';
 import { SDE } from '../urls';
 
-export const types = (state = [], action) => {
+const all = (state = {}, action) => {
   switch (action.type) {
     case type.SET_TYPES:
       return action.types;
@@ -13,6 +14,17 @@ export const types = (state = [], action) => {
       return state;
   }
 };
+
+const selected = (state = null, action) => {
+  switch (action.type) {
+    case type.SELECT_TYPE:
+      return action.typeID;
+    default:
+      return state;
+  }
+}
+
+export const types = combineReducers({ all, selected });
 
 export const typesEpic = (action$) => action$.pipe(
   ofType(type.GET_TYPES),
